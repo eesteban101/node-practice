518309371ec5e7d5f546e6e0eca88ff54def78e1
eesteban101 <49514313+eesteban101@users.noreply.github.com>
2019-06-26 23:03:50 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 /*server.js*/
-const http = require('http');
 var express = require('express');
-var app = express();
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end();
-});
-server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
-});
+var path = require('path');
+var app = new express();
+var port = 3000;
 
-app.get(“/home”,	(req,	res)	=>{
-res.sendFile(path.join(__dirname	+	‘/home.html’));
+app.get("/",	(req,	res)	=>{
+res.sendFile(path.join(__dirname +	"/home.html"));
 })
 
-app.get(“/about”,	(req,	res)	=>{
-res.sendFile(path.join(__dirname	+	‘/about.html’));
+app.get("/about",	(req,	res)	=>{
+res.sendFile(path.join(__dirname +	"/about.html"));
 })
 
-app.get(“/contact”,	(req,	res)	=>{
-res.sendFile(path.join(__dirname	+	‘/contact.html’));
+app.get("/contact",	(req,	res)	=>{
+res.sendFile(path.join(__dirname +	"/contact.html"));
 })
+
+app.get("/error", (req, res)     =>{
+res.sendFile(path.join(__dirname + "/bogos.html"));
+})
+
+app.listen(port);
